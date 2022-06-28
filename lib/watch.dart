c32b1505ff9f5e75cd4a3031e15327862e4bc3a5
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-06-29 07:58:12 +0800

    1.0.7

--- lib/watch.dart
+++ lib/watch.dart
@@ -9,13 +9,20 @@ class Watch<T> {
   // properties
   T get value => _value;
   Stream<T> get stream => _controller.stream;
-  bool _called = false;
+
+  /// Allows you to control the values which are allowed inside of this watch
+  /// useful for clamping values etc
+  T Function(T t)? clamp;
 
   bool isNullable<T>() => null is T;
 
   Watch(this._value, {
-    void Function(T t)? onChanged
+    void Function(T t)? onChanged,
+    this.clamp,
   }){
+    if (clamp != null){
+        this._value = clamp!(_value);
+    }
     if (onChanged != null){
       this.onChanged(onChanged);
     }
@@ -23,9 +30,14 @@ class Watch<T> {
 
   set value(T t){
     if (_value == t) return;
+
+    if (clamp != null){
+      t = clamp!(t);
+    }
     _value = t;
     _controller.add(value);
   }
+  
 
   void call(T t){
     value = t;
